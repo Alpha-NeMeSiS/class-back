eed436b35a61c4f4b54f95fb801b97f4e8e775fb
Eliot DE LEMOS <edelemos@edutice.lan>
2025-02-14 13:13:23 +0100

    user controler

--- WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2978e29d9947bcf1ce1278bb4ded1e49b79b443d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b75c892734ad6a5c853d25aec385f7483525a600")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
