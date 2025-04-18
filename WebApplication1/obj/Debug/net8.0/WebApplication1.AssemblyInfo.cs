67f4e6500dfc24a7353fa4e292211648ec76209e
Eliot <eliot.delemos@gmail.com>
2025-04-18 13:37:03 +0200

    commit cache

--- WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2978e29d9947bcf1ce1278bb4ded1e49b79b443d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7ea66f87bbc5158edcf14f27a751c347d0bd4964")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
